d0faef0f4a8a3f9dcd239827024ff3761a66486d
Fajar Prasetyo <fajartru1515@gmail.com>
2023-09-13 17:28:00 +0700

    portfolio

--- dist/js/script.js
+++ dist/js/script.js
@@ -20,3 +20,22 @@ hamburger.addEventListener('click', function() {
     navMenu.classList.toggle('hidden');
 });
    
+// DARK MODE
+const darkToggle = document.querySelector('#dark-toggle')
+const html  = document.querySelector('html')
+
+darkToggle.addEventListener('click',function(){
+    if (darkToggle.checked){
+        html.classList.add('dark');
+        localStorage.theme = 'dark';
+    } else {
+    html.classList.remove('dark');
+    localStorage.theme = 'light';
+    }
+});
+
+if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    darkToggle.checked = true;
+  } else {
+    darkToggle.checked = false;
+  }
